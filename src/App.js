ebdc9b9e6c30ded36f35d03cf6e7429355857c7d
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-25 14:14:22 +0530

    added switch before routes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import AllProducts from "./components/AllProducts"
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import SingleProduct from "./components/SingleProduct"
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Route path="/" exact>
-            <AllProducts />
-          </Route>
-          <Route path="/product/:id" exact component={SingleProduct}>
-          </Route>
-          <Footer />
-        </div>
+        <Switch>
+          <div className="App">
+            <Header />
+            <Route path="/" exact>
+              <AllProducts />
+            </Route>
+            <Route path="/product/:id" exact component={SingleProduct}>
+            </Route>
+            <Footer />
+          </div>
+        </Switch>
       </BrowserRouter>
     )
   }
